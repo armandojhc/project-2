42e4d62a73d64fcad2d5196a696ebdc0e7510d31
Alan Margolies <amargolies88@gmail.com>
2020-03-12 21:04:11 -0400

    completed minimum server code

--- server.js
+++ server.js
@@ -3,10 +3,18 @@ let PORT = process.env.PORT || 8080;
 let app = express();
 
 app.use(express.static("public"));
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 let exphbs = require("express-handlebars");
 
-app.engine("handlebars", exphbs({ defaultLayout: "main"}));
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
+
+let routes = require("./controllers/trioController.js");
+
+app.use(routes);
+
+app.listen(PORT, () => {
+    console.log("Server listening on: http://localhost:" + PORT);
+});
